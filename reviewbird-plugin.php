4ac4116cd0669014e4d7fb62b8e719da6091294f
Clifton Griffin <clif@objectiv.co>
2025-10-30 17:44:24 -0400

    Declare HPOS compatibility

--- reviewbird-plugin.php
+++ reviewbird-plugin.php
@@ -142,6 +142,15 @@ register_activation_hook( __FILE__, 'activate_reviewbird' );
 register_deactivation_hook( __FILE__, 'deactivate_reviewbird' );
 
 /**
+ * Declare compatibility with WooCommerce features.
+ */
+add_action( 'before_woocommerce_init', function() {
+	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+	}
+} );
+
+/**
  * Begins execution of the plugin.
  */
 function run_reviewbird() {
