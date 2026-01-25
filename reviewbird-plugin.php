7557a362c84d0924d7f07e8519476330def9d392
Clifton Griffin <clif@objectiv.co>
2026-01-24 19:59:43 -0500

    Update version numbers.

--- reviewbird-plugin.php
+++ reviewbird-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: reviewbird
  * Plugin URI: https://www.reviewbird.com
  * Description: Automated review collection that blocks spam, catches complaints, and showcases your best feedback.
- * Version: 1.0.6
+ * Version: 1.0.7
  * Author: reviewbird
  * Author URI: https://reviewbird.com
  * Text Domain: reviewbird-reviews
@@ -32,7 +32,7 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * Currently plugin version.
  */
-define( 'REVIEWBIRD_VERSION', '1.0.6' );
+define( 'REVIEWBIRD_VERSION', '1.0.7' );
 
 /**
  * Plugin directory path.
