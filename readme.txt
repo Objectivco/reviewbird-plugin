01a8901951f8a3f1fe0bd8ade5d5dd105d1f2255
Clifton Griffin <clif@objectiv.co>
2026-07-02 15:28:48 -0400

    Update tested versions.

--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: reviewbird, clifgriffin
 Tags: reviews, woocommerce, product reviews, ratings, customer reviews
 Requires at least: 5.0
-Tested up to: 6.9
+Tested up to: 7.0
 Stable tag: 1.0.20
 Requires PHP: 7.4
 License: GPLv2 or later
--- reviewbird-plugin.php
+++ reviewbird-plugin.php
@@ -11,7 +11,7 @@
  * Tested up to: 6.9
  * Requires PHP: 7.4
  * WC requires at least: 5.0
- * WC tested up to: 8.4
+ * WC tested up to: 10.9.1
  * Requires Plugins: woocommerce
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
