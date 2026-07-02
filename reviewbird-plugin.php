471b8047057d06ff5800cb8d21d1a3301b4454d8
Clifton Griffin <clif@objectiv.co>
2026-07-02 15:30:12 -0400

    Update another version

--- reviewbird-plugin.php
+++ reviewbird-plugin.php
@@ -7,7 +7,7 @@
  * Author: Reviewbird
  * Text Domain: reviewbird
  * Domain Path: /languages
- * Requires at least: 5.0
+ * Requires at least: 5.9
  * Tested up to: 6.9
  * Requires PHP: 7.4
  * WC requires at least: 5.0
